ca217ed229786caba046a448cb12ae6c959abae6
Ilham-Nur <ilonoer26@gmail.com>
2026-05-19 18:10:13 +0700

    Apply DompetArc branding

--- front-end/capacitor.config.ts
+++ front-end/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.arcdev.keuangan',
-  appName: 'Keuangan',
+  appName: 'DompetArc',
   webDir: 'build'
 };
 
